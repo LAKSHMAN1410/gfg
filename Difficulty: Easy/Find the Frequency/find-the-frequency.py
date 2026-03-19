8fc3c0b2e32f5fba80120f8e7f8294353fd58732
LAKSHMANAN.S <sivalakshmansivan@gmail.com>
2026-03-19 21:44:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the Frequency/find-the-frequency.py
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.py
@@ -2,15 +2,11 @@
 You're given an array (arr)
 Return the frequency of element x in the given array
 """
+from collections import Counter
 class Solution:
     def findFrequency(self, arr, x):
         # code here
-        d={}
-        for n in arr:
-            if n in d:
-                d[n]+=1
-            else:
-                d[n]=1
+        d=Counter(arr)
         for k,v in d.items():
             if k==x:
                 return v 
